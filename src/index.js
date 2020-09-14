bc4132aeae967e25573a7a7d6a607786f5832e78
NedVill <liamholwett@gmail.com>
2020-09-14 21:34:07 +0300

    init: start typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+	if (!array || !array.length) {
+		return 0;
+	}
+	
+	return array.sort((a, b) => {
+		return a - b;
+	})[0]
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+	if (!array || !array.length) {
+		return 0;
+	}
+	
+	return array.sort((a, b) => {
+		return a - b;
+	})[array.length - 1]
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+	if (!array || !array.length) {
+		return 0;
+	}
+
+	let sortedArr = array.sort((a, b) => {
+		return a - b;
+	})
+
+	return sortedArr.reduce((a, b) => {
+		return a + b;
+	}) / sortedArr.length
 }
